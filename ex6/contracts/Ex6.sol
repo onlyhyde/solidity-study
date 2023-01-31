4aa985468beb007bf64b33f67b59ed06c5e9f101
H.kevin <mh.ha@artdefinance.io>
2023-01-31 20:48:44 +0900

    feat : Ex6
    - pure / view
    - memory / storage

---
+++ ex6/contracts/Ex6.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract PureView {
+  uint8 private data = 255;
+
+  function getData() public view returns(uint8) { // state값을 조회하는 경우
+    return data;
+  }
+
+  function getPureData() public pure returns(uint8) { // state값을 조회 하지 않은 경우
+    uint8 tempData = 1;
+    return tempData;
+  }
+
+  // Storage : 영구적 저장 
+  // memory : 일시적 생성 
+  uint8 storage private sdata = 255;
+
+  function getStorageData() public view returns(uint8) { // state값을 조회하는 경우
+    return sdata;
+  }
+
+  function getMemoryData(string memory _data) public pure returns(uint8 memory) { // state값을 조회 하지 않은 경우
+    uint8 memory tempData = 1;
+    return tempData;
+  }
+
+  // memory 값을 읽을때는 pure를 사용, storage 값을 읽을때는 view를 사용.
+}
---
+++ ex6/migrations/1_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Ex6 = artifacts.require('Ex6');
+
+module.exports = function (deployer) {
+  deployer.deploy(Ex6);
+};

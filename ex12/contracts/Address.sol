7ff0c5294a422e6ac811815537395a44f58e2434
H.kevin <mh.ha@artdefinance.io>
2023-02-01 15:42:49 +0900

    feat : Ex12
    - Address Data Type
    - This

---
+++ ex12/contracts/Address.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Address {
+  //address public failUser=0xdc3bab6b6af0773a1bf2038fdc716dcd0bdbd564; // 소문자만
+  address public user=0xdC3BaB6B6aF0773a1bf2038fDc716dcD0bDbd564; // 정상적인 주소(대소문자 포함)
+  // 체크섬이 정상적이지 않은 주소는 Solidity에서 사용할 수 없음.
+
+  // address를 payable로 캐스팅해서 사용.
+  // 이더리움을 주고 받는 주소는 payable를 명시해줘야만 smartcontract에서 사용 가능.
+  address payable public payable_user = payable(user);
+
+  // Ex, contract가 이더를 전송받아서 다른 유저에게 주는 경우.
+  // 함수에도 payable을 붙여줘야 함.
+  function setEth() public payable {
+    //   user.transfer(1000000000); // Error, user는 address 
+      payable_user.transfer(1000000000); // 정상, payable_user는 address payable 
+  }
+
+  // Address type은 .balance를 통해 잔액정보를 알수있음.
+  function getBalance() public view returns(uint) {
+      return user.balance;
+  }
+}
---
+++ ex12/contracts/This.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract This {
+  // This는 Contract를 지칭한다. 
+  // 
+  function getBalance() public view returns(uint) {
+    return address(this).balance; // contract를 address형으로 캐스팅 후, balance를 읽어 Contract의 잔액을 읽수 있음.
+  }
+
+  // external은 외부에서만 접근되고, 내부에서는 접근 불가.
+  uint public data = 10;
+  function externalFunc() external {
+    data = 15;
+  }
+
+  function internalFunc() internal {
+    //externalFunc(); // Error, 내부에서 external 함수 접근 불가.
+    this.externalFunc(); // 정상동작. this는 Contract를 나타냄.
+  }
+}

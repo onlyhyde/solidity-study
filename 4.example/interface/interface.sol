c2cef9b29aa1553723c1b412a42a2a8a5eae21c1
onlyhyde <onlyhyde@sogang.ac.kr>
2023-07-27 07:25:29 +0900

    feat : add interface contract

---
+++ 4.example/interface/interface.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Counter {
+    uint public count;
+    function getCount() external view returns(uint) {
+        return count;
+    }
+    function inc() external {
+        count += 1;
+    }
+}
+
+interface ICounter {
+    function getCount() external view returns(uint);
+    function inc() external;
+}
+
+contract MyContract {
+    function incrementCount(address _counter) external {
+        ICounter(_counter).inc();
+    }
+    function getCount(address _counter) external view returns(uint) {
+        return ICounter(_counter).getCount();
+    }
+}

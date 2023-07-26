c31277dd3db82a42664bbf5b0c2473e900e99bca
onlyhyde <onlyhyde@sogang.ac.kr>
2023-07-27 07:25:13 +0900

    feat : add upgradable contract (ERC1967)

---
+++ 4.example/erc1967/UUPS-Proxy-V1.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract MyToken is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    uint256 public count;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {
+
+    }
+    
+    function inc() external {
+            count++;
+    }
+}

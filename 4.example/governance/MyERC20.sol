194d6adf872e9a8ba65ac5170b3bc6824e629961
0xtopaz <kevin.dev201@gmail.com>
2023-10-07 17:20:31 +0900

    feat : governance

---
+++ 4.example/governance/MyERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
+contract MyERC20 is ERC20PresetMinterPauser {
+    constructor() ERC20PresetMinterPauser("MyToken", "MT") {
+        // mint(msg.sender, 1 ether);
+    }
+}
+

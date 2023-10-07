c1200a66bb9bb7e5e1a8b1b7787eea115f7b3a6c
0xtopaz <kevin.dev201@gmail.com>
2023-10-07 17:20:05 +0900

    feat : erc721a

---
+++ 4.example/erc721a/My721ANFT2.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.4;
+
+import "https://github.com/chiru-labs/ERC721A/blob/main/contracts/ERC721A.sol";
+
+contract MyERC721A is ERC721A {
+    constructor() ERC721A("Myerc721A", "721A") {}
+
+    //Base URI 설정
+    function _baseURI() internal view virtual override returns (string memory) {
+        return 'ipfs://QmZcH4YvBVVRJtdn4RdbaqgspFU8gH6P9vomDpBVpAL3u4/';
+    }
+
+    //Azuki와 같이 유저가 이더리움을 내고 직접 민팅하는 함수를 가정하고 제작
+    // 개당 가격은 0.001 이더로 가정, 만약 정확한 수치가 아닐 경우 오류가 나도록 진행
+    function mint(uint256 quantity) external payable {
+        require(msg.value == (0.001 ether * quantity), "MyERC721A : msg.value is not correct");
+        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
+        _mint(msg.sender, quantity);
+    }
+}

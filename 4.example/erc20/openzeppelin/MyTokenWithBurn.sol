627a5342622220d7d4fc5a8de67cc9b1b75564df
0xtopaz <kevin.dev201@gmail.com>
2023-10-07 17:18:58 +0900

    feat : erc20 token

--- 4.example/erc20/openzeppelin/MyTokenWithBurn.sol
+++ 4.example/erc20/openzeppelin/MyTokenWithBurn.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MyTokenWihtMint is ERC20, Ownable {
+contract MyTokenWihtMint is ERC20, Ownable, ERC20Burnable {
     uint8 private _decimals = 6;
 
     constructor() ERC20("MyToken", "TTT") {
@@ -17,5 +18,21 @@ contract MyTokenWihtMint is ERC20, Ownable {
 
     function decimals() public view override  returns (uint8) {
         return _decimals;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }

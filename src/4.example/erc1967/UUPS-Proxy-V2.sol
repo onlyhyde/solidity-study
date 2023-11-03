dadd8ac76e2a6b235678f1d3d15eff837655bd7b
0xTopaz <myunghwan@imantisco.com>
2023-11-03 16:42:59 +0900

    fix : modified build fail issue on foundry

--- src/4.example/erc1967/UUPS-Proxy-V2.sol
+++ src/4.example/erc1967/UUPS-Proxy-V2.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract MyToken is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -13,21 +13,15 @@ contract MyToken is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         _disableInitializers();
     }
 
-    function initialize() initializer public {
-        __Ownable_init();
+    function initialize() public initializer {
+        __Ownable_init(_msgSender());
         __UUPSUpgradeable_init();
     }
 
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        onlyOwner
-        override
-    {
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    }
-    
     function inc() external {
-            count++;
+        count++;
     }
 }
 
--- src/4.example/erc20/openzeppelin/MyERC20-Burn.sol
+++ src/4.example/erc20/openzeppelin/MyERC20-Burn.sol
@@ -4,13 +4,14 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract MyERC20 is ERC20, Ownable, ERC20Burnable, Pausable {
     uint8 private _decimals = 6;
-    // 1000000000 * 10 ** 18  
+    // 1000000000 * 10 ** 18
     // 1 ETH = 1 * 10 ** 18 (Wei)
-    constructor() ERC20("MyERC20Token", "METT") Ownable() {
+
+    constructor() ERC20("MyERC20Token", "METT") Ownable(_msgSender()) {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
@@ -28,12 +29,5 @@ contract MyERC20 is ERC20, Ownable, ERC20Burnable, Pausable {
 
     function unpause() public onlyOwner {
         _unpause();
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
